7a8c8594c7e941242785a52745ed05798369062f
YoYa <unrealboundary@gmail.com>
2019-03-04 21:08:47 +0800

    JavaWeb

--- src/main/java/servlet/GeroageServlet.java
+++ src/main/java/servlet/GeroageServlet.java
@@ -2,6 +2,7 @@
 package servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.GenericServlet;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
@@ -11,7 +12,9 @@ public class GeroageServlet extends GenericServlet{
 
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
-        
+        PrintWriter out = res.getWriter();//連接(HTTP) message body 第四層
+        out.print("Hello GeroageServlet ");
+        out.print("Hello GeroageServlet 111222333444");
     }
     
 }
