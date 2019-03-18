af9757149499348a2dd97dd75432abd0c7e9a7eb
Yoya <unrealboundary@gmail.com>
2019-03-18 20:54:14 +0800

    預先載入設定
    loadOnStartup , init()

--- src/main/java/servlet/BaseHttpServlet.java
+++ src/main/java/servlet/BaseHttpServlet.java
@@ -2,17 +2,21 @@ package servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "BaseHttpServlet", urlPatterns = {"/servlet/MyHttpServlet"})
+@WebServlet(name = "BaseHttpServlet", urlPatterns = {"/servlet/MyHttpServlet"}, loadOnStartup = 1)
 public class BaseHttpServlet extends HttpServlet {
 
+    @Override
+    public void init() throws ServletException {
+        System.out.println("init()...");
+    }
+
+    
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter out = resp.getWriter();
