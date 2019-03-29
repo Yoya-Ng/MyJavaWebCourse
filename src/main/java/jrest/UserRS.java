17f11da0563c3db3320b6ae575c712cf3605f307
Yoya <unrealboundary@gmail.com>
2019-03-29 21:42:49 +0800

    PUT 拆字串 也可取得 ＠FormParam

--- src/main/java/jrest/UserRS.java
+++ src/main/java/jrest/UserRS.java
@@ -13,43 +13,61 @@ import javax.ws.rs.PathParam;
 
 @Path("/user")
 public class UserRS {
+
     private UserDao dao;
 
     // 預設建構值 會自動加入到所有建構值中
     {
         dao = new UserDao();
     }
-    
+
     @GET
     @Path("/{id}")
     public String get(@PathParam("id") int id) {
         return new Gson().toJson(dao.get(id));
     }
-    
+
     @GET
     @Path("/all")
     public String queryAll() {
         return new Gson().toJson(dao.queryAll());
     }
-    
+
     @DELETE
     @Path("/{id}")
     public String delete(@PathParam("id") int id) {
         dao.delete(id);
         return "Delete OK!";
     }
-    
+
     @POST
     public String create(@FormParam("username") String username) {
         dao.create(username);
         return "Create OK!";
     }
-    
+
     @PUT
     @Path("/{id}")
-    public String update(@PathParam("id") int id , String agrs) {
-        // args -> username = bob
-        dao.update(id, agrs.split("=")[1]);
-        return "Update OK!";
-    }
+    public String update(@PathParam("id") int id, @FormParam("newName") String newName) {
+        dao.update(id, newName); 
+        return "Update OK"; 
+    } 
+    
+//    @PUT
+//    @Path("/{id}")
+//    public String update(@PathParam("id") int id, String args) {
+//        // args -> newName=bob 
+//        String newName = getValue(args, "newName"); 
+//        dao.update(id, newName); 
+//        return "Update OK"; 
+//    } 
+     
+    private String getValue(String args, String name) { 
+        for(String arg : args.split("&")) { 
+            if(arg.split("=")[0].equals(name)) { 
+                return arg.split("=")[1]; 
+            } 
+        } 
+        return null; 
+    } 
 }
