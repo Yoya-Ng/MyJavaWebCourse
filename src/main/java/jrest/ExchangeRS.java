86768de0c1fb23a98fbbce10eb800d571be346a1
Yoya <unrealboundary@gmail.com>
2019-03-29 20:34:59 +0800

    .contains 字串判斷內容是否有 "=x"

--- src/main/java/jrest/ExchangeRS.java
+++ src/main/java/jrest/ExchangeRS.java
@@ -14,6 +14,9 @@ public class ExchangeRS {
     @Path("/{symbol}")
     //http://localhost:8084/MyJavaWebCourse/jrest/exchange/USDTWD=x 透過URL換匯
     public String sayStock(@PathParam("symbol") String symbol) {
+        //.contains 判斷內容是否有 "=x"
+        symbol = symbol.contains("=x")?symbol:symbol+"=x";
+        
         double price =0;
          try {
             Stock stock = YahooFinance.get(symbol);
@@ -22,4 +25,9 @@ public class ExchangeRS {
         }
         return symbol +" -> "+ price;
     }
+    
+//    測試
+//    public static void main(String[] args) {
+//        System.out.println( new ExchangeRS().sayStock("USDTWD=x"));
+//    }
 }
