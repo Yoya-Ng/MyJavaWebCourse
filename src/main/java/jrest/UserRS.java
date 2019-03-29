088b93c5af51c142c37ae899deb508c9133625dd
Yoya <unrealboundary@gmail.com>
2019-03-29 21:12:38 +0800

    User jersey All
    查詢全部 單一查詢

--- src/main/java/jrest/UserRS.java
+++ src/main/java/jrest/UserRS.java
@@ -1,21 +1,32 @@
-
 package jrest;
 
-import dao.UserDao;
+import com.google.gson.Gson;
 
+import dao.UserDao;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
+@Path("/user")
 public class UserRS {
     private UserDao dao;
-    
-     UserRS(){
-        // dao = new UserDao(); 會自動加入
-    }
-     
-     UserRS(int i ){
-        // dao = new UserDao(); 會自動加入
-    }
-    
+
     // 預設建構值 會自動加入到所有建構值中
     {
         dao = new UserDao();
     }
+    
+    @GET
+    @Path("/{id}")
+    public String get(@PathParam("id") int id) {
+        return new Gson().toJson(dao.get(id));
+    }
+    
+    @GET
+    @Path("/all")
+    public String queryAll() {
+        return new Gson().toJson(dao.queryAll());
+    }
+    
+    
 }
