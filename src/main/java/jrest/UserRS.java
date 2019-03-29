4b50e0b2abc55e049599654f15254d7f6c634dc5
Yoya <unrealboundary@gmail.com>
2019-03-29 21:13:00 +0800

    User jersey Delete

--- src/main/java/jrest/UserRS.java
+++ src/main/java/jrest/UserRS.java
@@ -3,6 +3,7 @@ package jrest;
 import com.google.gson.Gson;
 
 import dao.UserDao;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -28,5 +29,10 @@ public class UserRS {
         return new Gson().toJson(dao.queryAll());
     }
     
-    
+    @DELETE
+    @Path("/{id}")
+    public String delete(@PathParam("id") int id) {
+        dao.delete(id);
+        return "Delete OK!";
+    }
 }
