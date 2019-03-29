410b849ebc5c94c7085494b6d153a26c620c85bd
Yoya <unrealboundary@gmail.com>
2019-03-29 19:24:08 +0800

    RESTful web Services 練習

---
+++ src/main/java/jrest/HelloRS.java
@@ -0,0 +1,29 @@
+package jrest; 
+ 
+import java.util.ArrayList; 
+import java.util.HashSet; 
+import java.util.Random; 
+import java.util.Set; 
+import javax.ws.rs.GET; 
+import javax.ws.rs.Path; 
+ 
+@Path("/hello") 
+public class HelloRS { 
+     
+    @GET 
+    public String sayHello() { 
+        return "Hello !"; 
+    } 
+     
+    @GET 
+    @Path("/lotto") 
+    public String getLotto() { 
+        Set<Integer> set = new HashSet<>(); 
+        while(set.size() < 6) { 
+            set.add(new Random().nextInt(46) + 1); 
+        } 
+        return set.toString(); 
+    } 
+     
+     
+} 
--- src/main/java/jrest/MyApplication.java
+++ src/main/java/jrest/MyApplication.java
@@ -6,6 +6,6 @@ import org.glassfish.jersey.server.ResourceConfig;
 @ApplicationPath("jrest") 
 public class MyApplication extends ResourceConfig{ 
     public MyApplication(){ 
-         
+
     } 
 } 
