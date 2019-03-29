894637b6e53e91c44d9a260a1eff790211461abf
Yoya <unrealboundary@gmail.com>
2019-03-29 19:38:02 +0800

    RESTful 實作 BMI
    @FormParam -> 此參數為ＰＯＳＴ用
    @QueryParam("h") double height, @QueryParam("w") double weight -> 載入頁面參數

--- src/main/java/jrest/HelloRS.java
+++ src/main/java/jrest/HelloRS.java
@@ -1,29 +1,38 @@
-package jrest; 
- 
-import java.util.ArrayList; 
-import java.util.HashSet; 
-import java.util.Random; 
-import java.util.Set; 
-import javax.ws.rs.GET; 
-import javax.ws.rs.Path; 
- 
-@Path("/hello") 
-public class HelloRS { 
-     
-    @GET 
-    public String sayHello() { 
-        return "Hello !"; 
-    } 
-     
-    @GET 
-    @Path("/lotto") 
-    public String getLotto() { 
-        Set<Integer> set = new HashSet<>(); 
-        while(set.size() < 6) { 
-            set.add(new Random().nextInt(46) + 1); 
-        } 
-        return set.toString(); 
-    } 
-     
-     
-} 
+package jrest;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.QueryParam;
+
+@Path("/hello")
+public class HelloRS {
+
+    @GET
+    public String sayHello() {
+        return "Hello !";
+    }
+
+    @GET
+    @Path("/lotto")
+    public String getLotto() {
+        Set<Integer> set = new HashSet<>();
+        while (set.size() < 6) {
+            set.add(new Random().nextInt(46) + 1);
+        }
+        return set.toString();
+    }
+
+    @GET
+    @Path("/bmi")
+    //public double calcBMI( @FormParam ){ @FormParam 此參數為ＰＯＳＴ用
+    //http://localhost:8084/MyJavaWebCourse/jrest/hello/bmi?h=170&w=70 在網址輸入參數
+    public double calcBMI(@QueryParam("h") double height, @QueryParam("w") double weight) {
+        double bmi = weight/ Math.pow(height, 2);
+        return bmi;
+    }
+}
