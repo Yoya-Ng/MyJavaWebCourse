2ca3ab9c14f6545d31c5ef797a6deb4722e46abc
Yoya <unrealboundary@gmail.com>
2019-04-12 20:30:28 +0800

    Servlet 新增方法

--- src/main/java/myjpa/JPA_UserServlet.java
+++ src/main/java/myjpa/JPA_UserServlet.java
@@ -19,24 +19,42 @@ public class JPA_UserServlet extends HttpServlet {
         Gson gson = new Gson();
         PrintWriter out = resp.getWriter();
         String method = req.getParameter("method");
-
+        User user;
+        String name;
+        Long id;
+        int age;
         switch (method) {
             case "save": // ?method=save&name=Joe&age=28 
-                String name = req.getParameter("name");
-                int age = Integer.parseInt(req.getParameter("age"));
-                User user = new User();
+                name = req.getParameter("name");
+                age = Integer.parseInt(req.getParameter("age"));
+                user = new User();
                 user.setName(name);
                 user.setAge(age);
                 controller.save(user);
                 out.print("save ok");
                 break;
             case "update": // ?method=update&id=1&name=Joe&age=28 
+                id = Long.parseLong(req.getParameter("id"));
+                name = req.getParameter("name");
+                user = controller.findById(id);
+                user.setName(name);
+                controller.update(user);
+                out.print("update ok");
                 break;
             case "delete": // ?method=delete&id=1 
+                id = Long.parseLong(req.getParameter("id"));
+                user = controller.findById(id);
+                controller.delete(user);
+                out.print("delete ok");
                 break;
             case "findById": // ?method=findById&id=1 
+                id = Long.parseLong(req.getParameter("id"));
+                user = controller.findById(id);
+                out.print("delete ok" + user);
                 break;
             case "queryByAge": // ?method=queryByAge&age=20 
+                age = Integer.parseInt(req.getParameter("age"));
+                out.print(gson.toJson(controller.queryByAge(age)));
                 break;
             default:
                 out.print(gson.toJson(controller.queryAll()));
