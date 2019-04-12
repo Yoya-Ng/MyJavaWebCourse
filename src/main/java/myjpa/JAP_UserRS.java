af582dcdb080128730f2a9315f03414a4521a8fe
Yoya <unrealboundary@gmail.com>
2019-04-12 21:03:37 +0800

    Postman 使用測試方法（重要）方便
    一個前端網頁可以用來測試 Servlet ， 會實作 HTTP 的每個方法
    POST 資料格式要選擇 x-www-form-urlencoded

--- src/main/java/myjpa/JAP_UserRS.java
+++ src/main/java/myjpa/JAP_UserRS.java
@@ -1,16 +1,20 @@
-
 package myjpa;
 
 import com.google.gson.Gson;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 
+// 在 jrest 已經註冊了Application ，在網址是會加上 /jrest/jpa_user_rs
 @Path("/jpa_user_rs")
 public class JAP_UserRS {
-    
-    private JPA_UserController controller = new JPA_UserController(); 
-    
+
+    private JPA_UserController controller = new JPA_UserController();
+
     @GET
     @Path("/{id}")
     public String get(@PathParam("id") Long id) {
@@ -21,5 +25,14 @@ public class JAP_UserRS {
     @Path("/all")
     public String queryAll() {
         return new Gson().toJson(controller.queryAll());
+    }
+
+    @POST
+    public String create(@FormParam("name") String name,@FormParam("age") int age) {
+        User user = new User();
+        user.setName(name);
+        user.setAge(age);
+        controller.save(user);
+        return "Create OK!";
     }
 }
