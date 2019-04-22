9617fd3cef636d6c0cae6d003c387c52ce5752af
Yoya <unrealboundary@gmail.com>
2019-04-22 20:36:21 +0800

    多加了 session id

--- src/main/java/servlet/SessionServlet.java
+++ src/main/java/servlet/SessionServlet.java
@@ -14,9 +14,12 @@ public class SessionServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         HttpSession session = req.getSession();
-        resp.getWriter().print("create Session");
+        resp.getWriter().print("create Session ");
         if (session.isNew()) {
             System.out.println("New:" + session.getId());
+            resp.getWriter().print("<br>New session id:" + session.getId());
+        } else {
+            resp.getWriter().print("<br>session id:" + session.getId());
         }
     }
 
@@ -26,7 +29,8 @@ public class SessionServlet extends HttpServlet {
         if (session != null) {
             System.out.println("Del:" + session.getId());
             session.invalidate();
-            resp.getWriter().print("destroy Session");
+            resp.getWriter().print("destroy Session ");
+            resp.getWriter().print("<br>expired session id:" + session.getId());
         }
     }
 
