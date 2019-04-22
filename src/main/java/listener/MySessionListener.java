9c906f92afe322d9ff78fc0b774221e27a16ff4b
Yoya <unrealboundary@gmail.com>
2019-04-22 20:54:50 +0800

    改

--- src/main/java/listener/MySessionListener.java
+++ src/main/java/listener/MySessionListener.java
@@ -1,8 +1,10 @@
 package listener;
 
+import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
+@WebListener
 public class MySessionListener implements HttpSessionListener {
 
     public static int count;
